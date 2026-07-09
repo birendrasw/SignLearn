1aa50e7ec19f7f80087a09e85b20dbc78a71b5dc
birendrasw <birendraabi12@gmail.com>
2026-07-10 03:39:52 +0700

    Update: Fully port all HTML views (Dashboard, Learn, Achievements, Profile, Settings) and features to React App

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './style_old.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
